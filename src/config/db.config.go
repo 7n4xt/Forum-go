c0a65ae1ef1b29d1cb7737f23be80c0ebde11bbc
Victor <victor.uzodimma@ynov.com>
2025-05-23 12:36:03 +0200

    Add database configuration and initialization logic, including error handling for missing parameters and connection issues. Create initial service and environment configuration files.

---
+++ src/config/db.config.go
@@ -0,0 +1,38 @@
+package config 
+ 
+import ( 
+    "database/sql" 
+    "fmt"                    
+    "log"                    
+ 
+    _ "github.com/go-sql-driver/mysql"
+
+var DbContext *sql.DB 
+
+func InitDB() { 
+
+    user := GetEnvWithDefault("DB_USER", "") 
+    pwd  := GetEnvWithDefault("DB_PWD", "") 
+    host := GetEnvWithDefault("DB_HOST", "localhost") 
+    port := GetEnvWithDefault("DB_PORT", "3306") 
+    name := GetEnvWithDefault("DB_NAME", "") 
+ 
+   
+    if user == "" || name == "" { 
+        log.Fatalf("Erreur de connexion à la base de données : paramètres manquants") 
+    } 
+
+    connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, pwd, host, port, name) 
+
+    var err error 
+    DbContext, err = sql.Open("mysql", connectionString) 
+    if err != nil { 
+        log.Fatalf("Erreur d’ouverture de la connexion : %v", err) 
+    }
+
+err = DbContext.Ping() 
+if err != nil { 
+DbContext.Close() 
+log.Fatalf("Échec du ping vers MySQL : %v", err) 
+} 
+} 
