6b2f01a5f61cd32bb084142e3b5854b812c38185
Victor <victor.uzodimma@ynov.com>
2025-05-23 12:42:16 +0200

    Add initial environment configuration and database migration scripts for forum application

--- src/config/db.config.go
+++ src/config/db.config.go
@@ -1,38 +1,44 @@
-package config 
- 
-import ( 
-    "database/sql" 
-    "fmt"                    
-    "log"                    
- 
-    _ "github.com/go-sql-driver/mysql"
+package config
 
-var DbContext *sql.DB 
+import (
+	"database/sql"
+	"fmt"
+	"log"
 
-func InitDB() { 
+	_ "github.com/go-sql-driver/mysql" 
+)
 
-    user := GetEnvWithDefault("DB_USER", "") 
-    pwd  := GetEnvWithDefault("DB_PWD", "") 
-    host := GetEnvWithDefault("DB_HOST", "localhost") 
-    port := GetEnvWithDefault("DB_PORT", "3306") 
-    name := GetEnvWithDefault("DB_NAME", "") 
- 
-   
-    if user == "" || name == "" { 
-        log.Fatalf("Erreur de connexion à la base de données : paramètres manquants") 
-    } 
 
-    connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, pwd, host, port, name) 
+var DbContext *sql.DB
 
-    var err error 
-    DbContext, err = sql.Open("mysql", connectionString) 
-    if err != nil { 
-        log.Fatalf("Erreur d’ouverture de la connexion : %v", err) 
-    }
 
-err = DbContext.Ping() 
-if err != nil { 
-DbContext.Close() 
-log.Fatalf("Échec du ping vers MySQL : %v", err) 
-} 
-} 
+func InitDB() {
+	
+	user := GetEnvWithDefault("DB_USER", "")
+	pwd  := GetEnvWithDefault("DB_PWD", "")
+	host := GetEnvWithDefault("DB_HOST", "localhost")
+	port := GetEnvWithDefault("DB_PORT", "3306")
+	name := GetEnvWithDefault("DB_NAME", "")
+
+	
+	if user == "" || name == "" {
+		log.Fatalf("Erreur de connexion à la base de données : paramètres manquants")
+	}
+
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, pwd, host, port, name)
+
+	
+	var err error
+	DbContext, err = sql.Open("mysql", connectionString)
+	if err != nil {
+		log.Fatalf("Erreur d’ouverture de la connexion : %v", err)
+	}
+
+	
+	err = DbContext.Ping()
+	if err != nil {
+		
+		DbContext.Close()
+		log.Fatalf("Échec du ping vers MySQL : %v", err)
+	}
+}
--- src/config/env.config.go
+++ src/config/env.config.go
@@ -1,1 +1,27 @@
 package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnv() {
+	errLoad := godotenv.Load(".env")
+	if errLoad != nil {
+		log.Fatalf("Erreur chargement fichier d'environemment - Impossible de lancer le programme \n\t Erreur : %s", errLoad.Error())
+	}
+
+	if (GetEnvWithDefault("DB_NAME","") == "" || GetEnvWithDefault("DB_USER","") == "" ){
+		log.Fatal("Erreur fichier d'environnement - Impossible de lancer le programe \n\t Erreur : Fichier d'environnemnt incomplet")
+	}
+}
+
+func GetEnvWithDefault(key, defaultValue string) string{
+	envVar, envErr := os.LookupEnv(key)
+	if !envErr {
+		return defaultValue
+	}
+	return envVar
+}
